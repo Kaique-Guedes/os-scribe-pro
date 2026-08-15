298f9aca337fde10acf30f035dc22ec99e9dbf58
Kaique-Guedes <136267544+Kaique-Guedes@users.noreply.github.com>
2026-08-15 09:52:41 -0300

    Update types.ts

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -14,6 +14,7 @@ export type Database = {
           id: string;
           nome: string;
           email: string | null;
+          notif_atraso_email: boolean;
           created_at: string;
           updated_at: string;
         };
@@ -21,6 +22,7 @@ export type Database = {
           id: string;
           nome?: string;
           email?: string | null;
+          notif_atraso_email?: boolean;
           created_at?: string;
           updated_at?: string;
         };
@@ -28,6 +30,7 @@ export type Database = {
           id?: string;
           nome?: string;
           email?: string | null;
+          notif_atraso_email?: boolean;
           created_at?: string;
           updated_at?: string;
         };
